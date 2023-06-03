cbac0de5b961ddb977195463ef5bce06f53dce24
DanyloYamelynets <130572960+DanyloYamelynets@users.noreply.github.com>
2023-06-03 22:59:29 +0300

    tasks 1, 2, 3

---
+++ src/js/task-01.js
@@ -0,0 +1,6 @@
+const categoriesList = document.querySelectorAll('li.item');
+console.log(`Number of categories: ${ categoriesList.length }`);
+categoriesList.forEach((category) => {
+    console.log(`Category: ${category.firstElementChild.textContent}`);
+    console.log(`Elements: ${category.lastElementChild.children.length}`);
+})
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients');
+const liIngredients = ingredients.map((ingredient) => {
+  const li = document.createElement('li');
+  li.textContent = ingredient;
+  li.classList.toggle('item');
+  ingredientsList.append(li);
+});
+
+
+// console.log(li);
