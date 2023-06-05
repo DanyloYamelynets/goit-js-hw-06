090de40d021b527f3f0886d43e0fcd71faad78bb
DanyloYamelynets <130572960+DanyloYamelynets@users.noreply.github.com>
2023-06-05 22:43:28 +0300

    task 7 done

---
+++ src/js/task-07.js
@@ -0,0 +1,6 @@
+const textSize = document.querySelector("#font-size-control");
+const text = document.querySelector("#text");
+
+textSize.addEventListener("input", () => {
+  text.style.fontSize = `${textSize.value}px`;
+});
