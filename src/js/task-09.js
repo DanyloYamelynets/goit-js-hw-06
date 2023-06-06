cfffbe86bb76c79f047ee8701f6e80952d998c73
DanyloYamelynets <130572960+DanyloYamelynets@users.noreply.github.com>
2023-06-06 15:41:10 +0300

    task 9 done

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,4 +1,16 @@
+const myColor = document.querySelector(".color");
+const changeColorBtn = document.querySelector(".change-color");
+const body = document.querySelector("body");
+
+changeColorBtn.addEventListener("click", onChangeColor);
+
+function onChangeColor() {
+  body.style.backgroundColor = getRandomHexColor();
+  myColor.textContent = getRandomHexColor();
+}
+
 function getRandomHexColor() {
+  console.log(document.body);
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
