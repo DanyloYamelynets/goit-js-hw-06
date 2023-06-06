07afba37ecad8ea7fc1cd4d6bbd549ac6e1d86ad
DanyloYamelynets <130572960+DanyloYamelynets@users.noreply.github.com>
2023-06-06 13:32:14 +0300

    task 8 done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,17 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", onloginFormSubmit);
+
+function onloginFormSubmit(event) {
+  event.preventDefault();
+  const { email, password } = event.currentTarget.elements;
+  if (email.value === "" || password.value === "") {
+    return alert("All inputs should be filled");
+  }
+  const data = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(data);
+  event.currentTarget.reset();
+}
